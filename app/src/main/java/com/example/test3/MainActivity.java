f58d9693f1e6901c632c3f0d2b8e13bf03ce19d2
kurumi1210 <001210xh>
2019-10-05 12:48:53 +0800

    update

--- app/src/main/java/com/example/test3/MainActivity.java
+++ app/src/main/java/com/example/test3/MainActivity.java
@@ -7,65 +7,42 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    int amount = 1;
-    int basePrice = 5;
+    private EditText editUsername, editPassword;
+    private Button btnLogin;
+
+    private String username="xh1210", password="1234";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        setListener();
+    }
 
-        Button btnMinus = findViewById(R.id.btn_minus);
-        Button btnPlus = findViewById(R.id.btn_plus);
-        final TextView tvAmount = findViewById(R.id.tv_amount);
-        final CheckBox cbCream = findViewById(R.id.cb_cream);
-        final CheckBox cbC = findViewById(R.id.cb_cc);
-        final CheckBox cbVanilla = findViewById(R.id.cb_vanilla);
-        Button btnOrder = findViewById(R.id.btn_order);
-        final TextView tvThankyou = findViewById(R.id.tv_ty);
+    private void findViews(){
+        editUsername=findViewById(R.id.username);
+        editPassword=findViewById(R.id.pw);
+        btnLogin=findViewById(R.id.login);
+    }
 
-        btnMinus.setOnClickListener(new View.OnClickListener() {
+    private void setListener(){
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Do stuff
-                if (amount >  1) {
-                    amount--;
-                    tvAmount.setText(Integer.toString(amount));
+                String inputUsername=editUsername.getText().toString();
+                String inputPassword=editUsername.getText().toString();
+
+                if (inputUsername.equals(username) && inputPassword.equals(password)){
+                    Toast.makeText(MainActivity.this,"Login Succesful", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this,"Login Fail", Toast.LENGTH_SHORT).show();
                 }
-            }
-        });
-
-        btnPlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Do stuff
-                amount++;
-                tvAmount.setText(Integer.toString(amount));
-            }
-        });
-
-        btnOrder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int finalPrice = basePrice;
-                if(cbCream.isChecked()){
-                    finalPrice += 1;
-                }
-                if(cbC.isChecked()){
-                    finalPrice += 2;
-                }
-                if(cbVanilla.isChecked()){
-                    finalPrice += 3;
-                }
-
-                finalPrice += amount;
-
-                Log.d("Final Price", Integer.toString(finalPrice));
-
-                tvThankyou.setText("Thank for your order. \n The total price is $" + Integer.toString(finalPrice));
             }
         });
     }
